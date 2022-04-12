cb7989ae37963c7f70c958206c42b6f2e6b78a68
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-04-11 21:10:42 -0300

    req02 - adiciona props ao Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,49 +1,13 @@
-// 1.
-// Crie o formulário que será usado para adicionar cartas ao baralho
-// .
-// Crie um formulário que será utilizado para criar as cartas do seu baralho.
-// Crie um componente chamado Form dentro da pasta src/components.
-// Renderize o componente Form dentro do componente principal App.
-// Crie os seguintes itens dentro do component Form:
-// bulb Dica: Você pode criar um componente de input. Lembre-se de sempre ter uma
-// label associada para cada input.
-
-// um campo do tipo text que contenha o atributo data-testid="name-input".
-// Este campo será usado para inserir o nome da carta.
-
-// um campo do tipo textarea que contenha o atributo data-testid="description-input".
-// Este campo será usado para inserir a descrição da carta.
-
-// um campo do tipo number que contenha o atributo data-testid="attr1-input".
-// Este campo será usado para inserir o primeiro atributo da carta.
-// Ele é livre para você adicionar o atributo que mais combinar com o seu baralho.
-
-// um campo do tipo number que contenha o atributo data-testid="attr2-input".
-// Este campo será usado para inserir o segundo atributo da carta.
-// Ele é livre para você adicionar o atributo que mais combinar com o seu baralho.
-
-// um campo do tipo number que contenha o atributo data-testid="attr3-input".
-// Este campo será usado para inserir o terceiro atributo da carta.
-// Ele é livre para você adicionar o atributo que mais combinar com o seu baralho.
-
-// um campo do tipo text que contenha o atributo data-testid="image-input".
-// Este campo será usado para inserir o caminho para imagem da carta.
-
-// um campo do tipo select que contenha o atributo data-testid="rare-input".
-// Este campo será usado para inserir a raridade da carta e deverá ter as
-// options: normal, raro e muito raro (é importante que as opções estejam nessa ordem).
-
-// um campo do tipo checkbox que contenha o atributo data-testid="trunfo-input".
-// Este campo será usado para inserir se a carta é o Super Trunfo.
-
-// um button que contenha o atributo data-testid="save-button" e que tenha o texto "Salvar".
-
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class Form extends React.Component {
   render() {
-    // const { seiLa } = this.props;
+    const { cardName, cardDescription, cardAttr1 } = this.props;
+    const { cardAttr2, cardAttr3, cardImage } = this.props;
+    const { cardRare, cardTrunfo } = this.props;
+    // const { hasTrunfo } = this.props;
+    const { isSaveButtonDisabled, onInputChange, onSaveButtonClick } = this.props;
     return (
       <div>
         <form>
@@ -54,6 +18,8 @@ class Form extends React.Component {
               type="text"
               placeholder="Nome da Carta"
               data-testid="name-input"
+              value={ cardName }
+              onChange={ onInputChange }
             />
           </label>
           <label htmlFor="card-description">
@@ -62,6 +28,8 @@ class Form extends React.Component {
               name="card-description"
               placeholder="Descrição da Carta"
               data-testid="description-input"
+              value={ cardDescription }
+              onChange={ onInputChange }
             />
           </label>
           <label htmlFor="card-attr1">
@@ -71,6 +39,8 @@ class Form extends React.Component {
               type="number"
               placeholder="Atributo 1"
               data-testid="attr1-input"
+              value={ cardAttr1 }
+              onChange={ onInputChange }
             />
           </label>
           <label htmlFor="card-attr2">
@@ -80,6 +50,8 @@ class Form extends React.Component {
               type="number"
               placeholder="Atributo 2"
               data-testid="attr2-input"
+              value={ cardAttr2 }
+              onChange={ onInputChange }
             />
           </label>
           <label htmlFor="card-attr3">
@@ -89,6 +61,8 @@ class Form extends React.Component {
               type="number"
               placeholder="Atributo 3"
               data-testid="attr3-input"
+              value={ cardAttr3 }
+              onChange={ onInputChange }
             />
           </label>
           <label htmlFor="card-img">
@@ -98,11 +72,18 @@ class Form extends React.Component {
               type="text"
               placeholder="URL da imagem"
               data-testid="image-input"
+              value={ cardImage }
+              onChange={ onInputChange }
             />
           </label>
           <label htmlFor="raridade">
             Raridade
-            <select name="raridade" data-testid="rare-input">
+            <select
+              name="raridade"
+              data-testid="rare-input"
+              value={ cardRare }
+              onChange={ onInputChange }
+            >
               <option>normal</option>
               <option>raro</option>
               <option>muito raro</option>
@@ -113,115 +94,38 @@ class Form extends React.Component {
               name="trunfo"
               type="checkbox"
               data-testid="trunfo-input"
+              checked={ cardTrunfo }
+              onChange={ onInputChange }
             />
             Super Trybe Trunfo
           </label>
-          <button type="button" data-testid="save-button">Salvar</button>
+          <button
+            type="button"
+            data-testid="save-button"
+            disabled={ isSaveButtonDisabled }
+            onClick={ onSaveButtonClick }
+          >
+            Salvar
+          </button>
         </form>
       </div>
     );
   }
 }
-// Form.propTypes = {
-//   headline: PropTypes.string.isRequired,
-// };
+
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  // hasTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
 
 export default Form;
-
-// .
-
-// O que será verificado
-// Será validado se existe um input texto que possui o data-testid="name-input".
-// Será validado se existe um input textarea que possui o data-testid="description-input".
-// Será validado se existe um input number que possui o data-testid="attr1-input".
-// Será validado se existe um input number que possui o data-testid="attr2-input".
-// Será validado se existe um input number que possui o data-testid="attr3-input".
-// Será validado se existe um input texto que possui o data-testid="image-input".
-// Será validado se existe um input select que possui o data-testid="rare-input".
-// Será validado se existe um input checkbox que possui o data-testid="trunfo-input".
-// Será validado se existe um botão que possui o data-testid="save-button".
-
-// .
-// .
-// .
-
-// 2.
-// Adicione as props necessárias ao componente de formulário
-// .
-// O componente Form deverá receber as seguintes props:
-// cardName, uma string;
-// cardDescription, uma string;
-// cardAttr1, uma string;
-// cardAttr2, uma string;
-// cardAttr3, uma string;
-// cardImage, uma string;
-// cardRare, uma string;
-// cardTrunfo, um boolean;
-// hasTrunfo, um boolean;
-// isSaveButtonDisabled, um boolean;
-// onInputChange, uma callback;
-// onSaveButtonClick, uma callback;
-// As props do componente Form deverão ser utilizadas conforme o indicado abaixo:
-
-// Campo name-input: a propriedade value deve receber o valor da prop cardName e a prop
-// onChange deve receber o valor da prop onInputChange.
-
-// Campo description-input: a propriedade value deve receber o valor da prop cardDescription e
-// a prop onChange deve receber o valor da prop onInputChange.
-
-// Campo attr1-input: a propriedade value deve receber o valor da prop cardAttr1 e a prop
-// onChange deve receber o valor da prop onInputChange.
-
-// Campo attr2-input: a propriedade value deve receber o valor da prop cardAttr2 e a prop
-// onChange deve receber o valor da prop onInputChange.
-
-// Campo attr3-input: a propriedade value deve receber o valor da prop cardAttr3 e a prop
-// onChange deve receber o valor da prop onInputChange.
-
-// Campo image-input: a propriedade value deve receber o valor da prop cardImage e a prop
-// onChange deve receber o valor da prop onInputChange.
-
-// Campo rare-input: a propriedade value deve receber o valor da prop cardRare e a prop
-// onChange deve receber o valor da prop onInputChange.
-
-// Campo trunfo-input: a propriedade checked deve receber o valor da prop cardTrunfo e a
-// prop onChange deve receber o valor da prop onInputChange.
-
-// Botão save-button: a propriedade disabled deve receber o valor da prop isSaveButtonDisabled
-// e a prop onClick deve receber o valor da prop onSaveButtonClick.
-
-// Obs: por enquanto a prop hasTrunfo ainda não foi utilizada, mas não se preocupe,
-// pois ela será usada em breve.
-
-// O que será verificado
-// Será validado se o campo de nome recebe o valor da prop cardName e se a callback
-// onInputChange é chamada quando o campo sofre alguma alteração.
-
-// Será validado se o campo de descrição recebe o valor da prop cardDescription e se a
-// callback onInputChange é chamada quando o campo sofre alguma alteração.
-
-// Será validado se o campo do atributo 1 recebe o valor da prop cardAttr1 e se a callback
-// onInputChange é chamada quando o campo sofre alguma alteração.
-
-// Será validado se o campo do atributo 2 recebe o valor da prop cardAttr2 e se a callback
-// onInputChange é chamada quando o campo sofre alguma alteração.
-
-// Será validado se o campo do atributo 3 recebe o valor da prop cardAttr3 e se a callback
-// onInputChange é chamada quando o campo sofre alguma alteração.
-
-// Será validado se o campo de imagem recebe o valor da prop cardImage e se a callback
-// onInputChange é chamada quando o campo sofre alguma alteração.
-
-// Será validado se o campo de raridade recebe o valor da prop cardRare e se a callback
-// onInputChange é chamada quando o campo sofre alguma alteração.
-
-// Será validado se o campo de Super Trufo recebe o valor da prop cardTrunfo e se a
-// callback onInputChange é chamada quando o campo sofre alguma alteração.
-
-// Será validado se o botão de salvar é habilitado se o valor da prop isSaveButtonDisabled
-// for false.
-
-// Será validado se o botão de salvar é desabilitado se o valor da prop isSaveButtonDisabled
-// for true.
-
-// Será validado a callback onSaveButtonClick é chamada quando o botão é clicado.
