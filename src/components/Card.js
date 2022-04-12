3e7f59d4cf60330c4fa0aee70b3feb36ac3d31da
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-04-11 22:08:45 -0300

    req03 - cria e renderiza Card com as props

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,6 @@
 // Crie um componente com o nome Card na pasta src/components e
 // renderize-o no componente principal App. O componente Card deve receber
 // as seguintes props:
-
 // cardName, uma string;
 // cardDescription, uma string;
 // cardAttr1, uma string;
@@ -12,6 +11,42 @@
 // cardRare, uma string;
 // cardTrunfo, um boolean;
 // Renderize o componente Card dentro do componente principal App.
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Card extends React.Component {
+  render() {
+    const { cardName, cardDescription, cardAttr1, cardTrunfo } = this.props;
+    const { cardAttr2, cardAttr3, cardImage, cardRare } = this.props;
+    return (
+      <div>
+        <p data-testid="name-card">{cardName}</p>
+        <img src={ cardImage } alt={ cardName } data-testid="image-card" />
+        <p data-testid="description-card">{cardDescription}</p>
+        <p data-testid="attr1-card">{cardAttr1}</p>
+        <p data-testid="attr2-card">{cardAttr2}</p>
+        <p data-testid="attr3-card">{cardAttr3}</p>
+        <p data-testid="rare-card">{cardRare}</p>
+        {
+          cardTrunfo && <p data-testid="trunfo-card">Super Trunfo</p>
+        }
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+};
+
+export default Card;
 
 // Exiba o valor da prop cardName. Você pode usar qualquer tag HTML que faça sentido,
 // desde que ela tenha o atributo data-testid="name-card".
